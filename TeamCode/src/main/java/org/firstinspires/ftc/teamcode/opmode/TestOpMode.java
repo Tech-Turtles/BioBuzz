fee1fb031ce6243c48a573d1d5c1ca87ae2fdacb
Brantley Lane <brantleyblane@gmail.com>
2026-09-16 21:27:14 -0500

    Added some comments to TestOpMode.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TestOpMode.java
@@ -8,6 +8,7 @@ public class TestOpMode extends OpModeEx {
     @Override
     public void init() {
         super.init();
+        // Let users know as soon as the robot is initialized!
         telemetry.addData("Status", "Initialized!");
         telemetry.update();
     }
@@ -15,6 +16,7 @@ public class TestOpMode extends OpModeEx {
     @Override
     public void start() {
         super.start();
+        // Let users know as soon as the robot starts!
         telemetry.addData("Status", "Started!");
         telemetry.update();
     }
@@ -22,6 +24,7 @@ public class TestOpMode extends OpModeEx {
     @Override
     public void loop() {
         super.loop();
+        // Print human-friendly odometry readout to telemetry
         telemetry.addData("Odometry", pinpointReadout.toString());
     }
 }
