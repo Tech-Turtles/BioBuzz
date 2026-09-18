fef541c21ab0bd4e6cb3b8f566135e146b79b0d3
Brantley Lane <brantleyblane@gmail.com>
2026-09-17 21:02:34 -0500

    Just added a package for control and an input class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Input.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.control;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Input {
+
+    // Declare gamepads
+    public Gamepad gamepad1;
+    public Gamepad gamepad2;
+
+    public Input(Gamepad gamepad1, Gamepad gamepad2) {
+
+    }
+
+}
