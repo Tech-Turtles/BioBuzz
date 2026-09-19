32400684eb20b97aea1aa053f6ae97c89204897f
Brantley Lane <brantleyblane@gmail.com>
2026-09-18 20:47:44 -0500

    Added some architecture for programs. Working on AutonomousEx.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousEx.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
-public class AutonomousEx {
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.opmode.OpModeEx;
+
+public class AutonomousEx extends OpModeEx {
+    // create timer object
+    public ElapsedTime timer;
+    // create object for time of autonomous match
+    public double autonomousLength = 30;
+
+    @Override
+    public void init() {
+        super.init();
+        // create timer
+        timer = new ElapsedTime();
+    }
+
+    @Override
+    public void start() {
+        timer.reset();
+    }
 }
